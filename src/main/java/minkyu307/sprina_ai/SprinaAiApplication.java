591c6e2edc42e17fac2104730e598501dd26f520
Minkyu Kim <82827785+minkyu307@users.noreply.github.com>
2026-01-18 14:28:33 +0900

    re-init

--- src/main/java/minkyu307/sprina_ai/SprinaAiApplication.java
+++ src/main/java/minkyu307/sprina_ai/SprinaAiApplication.java
@@ -1,24 +1,13 @@
-<<<<<<<< HEAD:src/main/java/minkyu307/sprina_ai/SprinaAiApplication.java
 package minkyu307.sprina_ai;
-========
-package minkyu4kim.DAS;
->>>>>>>> 571f4eb7526302d6cf629812e9676e52df56d22b:DAS/src/main/java/minkyu4kim/DAS/DasApplication.java
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-<<<<<<<< HEAD:src/main/java/minkyu307/sprina_ai/SprinaAiApplication.java
 public class SprinaAiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SprinaAiApplication.class, args);
-========
-public class DasApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DasApplication.class, args);
->>>>>>>> 571f4eb7526302d6cf629812e9676e52df56d22b:DAS/src/main/java/minkyu4kim/DAS/DasApplication.java
 	}
 
 }
--- src/test/java/minkyu307/sprina_ai/SprinaAiApplicationTests.java
+++ src/test/java/minkyu307/sprina_ai/SprinaAiApplicationTests.java
@@ -1,18 +1,10 @@
-<<<<<<<< HEAD:src/test/java/minkyu307/sprina_ai/SprinaAiApplicationTests.java
 package minkyu307.sprina_ai;
-========
-package minkyu4kim.DAS;
->>>>>>>> 571f4eb7526302d6cf629812e9676e52df56d22b:DAS/src/test/java/minkyu4kim/DAS/DasApplicationTests.java
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-<<<<<<<< HEAD:src/test/java/minkyu307/sprina_ai/SprinaAiApplicationTests.java
 class SprinaAiApplicationTests {
-========
-class DasApplicationTests {
->>>>>>>> 571f4eb7526302d6cf629812e9676e52df56d22b:DAS/src/test/java/minkyu4kim/DAS/DasApplicationTests.java
 
 	@Test
 	void contextLoads() {
